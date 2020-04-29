b61f098ef8edf2b86f6af441dee326f86db317d1
gines <gines.lopez@gmail.com>
2020-04-29 13:14:27 +0200

    cors

--- server/server.js
+++ server/server.js
@@ -8,6 +8,11 @@ const bodyParser = require('body-parser');
 
 app.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 // parse application/x-www-form-urlencoded
